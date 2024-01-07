b4fec0870d5a48eedddb1005cf3c4a86c6063750
NiloofarMeftahi <niloofar.m1991@gmail.com>
2024-01-07 12:16:04 +0200

    Add project files

---
+++ backend/functions/openai_requests.py
@@ -0,0 +1,24 @@
+import openai
+from decouple import config
+
+
+#Retrieve Enrivonment Variables
+openai.organization = config("OPEN_AI_ORG")
+openai.api_key = config("OPEN_AI_KEY")
+
+#Open AI - Whisper
+#Convert Audio to Text
+
+def convert_audio_to_text(audio_file):
+    try:
+        # transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        transcript = openai.audio.translations.create(
+        model="whisper-1", 
+        file=audio_file
+        )
+        message_text = transcript("text")
+       
+        return message_text
+    except Exception as e:
+        print(e)
+        return
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+    <div className=''>
+      <div className=" text-2xl bg-blue-300">Hello</div>
+    </div>
+  )
+}
+
+export default App
